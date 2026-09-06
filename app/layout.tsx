be999e999ab757df08704811e4f1bca7686462f9
nmpc2000 <nuno.miguelpc54@gmail.com>
2026-09-06 13:51:05 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { acceptInvite } from "./teams/actions";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  await acceptInvite();
-
   return (
     <html lang="pt">
       <body className={inter.className}>{children}</body>
